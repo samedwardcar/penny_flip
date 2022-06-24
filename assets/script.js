557aeaaad0469fabc0683f30b56b03001f867e76
samedwardcar <106223288+samedwardcar@users.noreply.github.com>
2022-06-23 21:25:02 -0500

    my starter js

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+let headsCount
+
+let flipButton = document.querySelector("#flip-btn");
+
+
+flipButton.addEventListener("click"), function(){
+    //random 
+    let isHeads = Math.random() <0.5
+    if(isHeads) {
+        document.querySelector("img").src = "./assets/images/penny-heads.jpg";
+        document.querySelector(".result-header");
+     } else(){
+        document.querySelector("img").src ="./assets/images/penny-heads.jpg"}
+        
+     }
+
+
+     document.querySelector(".heads-count")
+
+
+
+     document.querySelector("#clear-btn").addEventListener
+
+
+     function scoreTable ()
